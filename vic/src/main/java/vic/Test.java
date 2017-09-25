fd30d3f3ab4377e48453a19be5fda9d6ae7c0ab2
HP <HP@DESKTOP-DAHC9SJ>
2017-09-25 21:37:57 +0800

    fisrt time to modify

--- vic/src/main/java/vic/Test.java
+++ vic/src/main/java/vic/Test.java
@@ -4,7 +4,8 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("123");
 		System.out.println("321321");
-		System.out.println("ggt");
+		System.out.println("5553");
+		System.out.println("ggtaadas");
 		 
 	}
 }
